0c8eb281b36f0dafddba9edae88cec690c5d07ce
Torh-21 <majarotoheeb@gmail.com>
2024-01-08 14:44:01 -0800

    State task added

---
+++ api/v1/views/states.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+"""Module state.py: starts all routes with `state`"""
+from api.v1.views import app_views
+from models.state import State
+from models import storage
+from flask import jsonify, request, abort
+
+
+@app_views.route('/states',
+                 methods=['GET', 'POST'],
+                 strict_slashes=False)
+def states():
+    """returns all state object"""
+    if request.method == 'POST':
+        response = request.get_json()
+
+        if response is None:
+            abort(400, description='Not a JSON')
+
+        if 'name' not in response.keys():
+            abort(400, description='Missing name')
+        new_state = State(**response)
+        new_state.save()
+        return(new_state.to_dict()), 201
+
+    objs = [state.to_dict() for state in storage.all(State).values()]
+    return jsonify(objs)
+
+
+@app_views.route('/states/<state_id>',
+                 methods=['GET', 'DELETE', 'PUT'],
+                 strict_slashes=False)
+def state(state_id):
+    """returns a state based on the id"""
+
+    state = storage.get(State, state_id)
+    if state is None:
+        abort(404)
+
+    if request.method == 'DELETE':
+        state.delete()
+        storage.save()
+        return '{}'
+
+    if request.method == 'PUT':
+        response = request.get_json()
+        if response is None:
+            abort(400, description='Not a JSON')
+        for k, v in response.items():
+            if k.endswith('ated_at') or k == 'id':
+                continue
+            setattr(state, k, v)
+        state.save()
+    return jsonify(state.to_dict())
